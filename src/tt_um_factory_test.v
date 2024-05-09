4d2e8fe2c01ba870ecc29fd25233c36804c5a8b3
Uri Shaked <uri@urishaked.com>
2024-05-10 00:58:42 +0300

    style: reformat code with verible-verilog-format

--- src/tt_um_factory_test.v
+++ src/tt_um_factory_test.v
@@ -9,40 +9,32 @@
 `default_nettype none
 
 module tt_um_factory_test (
-	input  wire [7:0] ui_in,	// Dedicated inputs
-	output wire [7:0] uo_out,	// Dedicated outputs
-	input  wire [7:0] uio_in,	// IOs: Input path
-	output wire [7:0] uio_out,	// IOs: Output path
-	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
-	reg rst_n_i;
-	reg [7:0] cnt;
+  reg rst_n_i;
+  reg [7:0] cnt;
 
-	always @(posedge clk or negedge rst_n)
-		if (~rst_n)
-			rst_n_i <= 1'b0;
-		else
-			rst_n_i <= 1'b1;
+  always @(posedge clk or negedge rst_n)
+    if (~rst_n) rst_n_i <= 1'b0;
+    else rst_n_i <= 1'b1;
 
-	always @(posedge clk or negedge rst_n_i)
-		if (~rst_n_i)
-			cnt <= 0;
-		else
-			cnt <= cnt + 1;
-	
-	assign uo_out  = ui_in[0] ? cnt : uio_in;
-	assign uio_out = ui_in[0] ? cnt : 8'h00;
-	assign uio_oe  = ui_in[0] ? 8'hff : 8'h00;
+  always @(posedge clk or negedge rst_n_i)
+    if (~rst_n_i) cnt <= 0;
+    else cnt <= cnt + 1;
 
-	// avoid linter warning about unused pins:
-	wire _unused_pins = &{ 
-		ena, 
-		ui_in[7:1],
-		1'b1
-	};
+  assign uo_out  = ui_in[0] ? cnt : uio_in;
+  assign uio_out = ui_in[0] ? cnt : 8'h00;
+  assign uio_oe  = ui_in[0] ? 8'hff : 8'h00;
 
-endmodule // tt_um_factory_test
+  // avoid linter warning about unused pins:
+  wire _unused_pins = &{ena, ui_in[7:1], 1'b1};
+
+endmodule  // tt_um_factory_test
